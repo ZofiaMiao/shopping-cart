76ccbcca696ea6af2025fdf464893070ca04f6ce
zofina <izhmaple@163.com>
2017-05-23 16:23:38 +0800

    将原始html备份

---
+++ Developed/js/cart.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zofia on 17/5/23.
+ */
+var vm = new Vue({
+    el:"#app",
+    ata:{
+    //数据
+
+    },
+    fiters:{
+    //过滤器
+        
+    },
+    mounted:function () {
+    //挂载到实例上去之后调用该钩子
+
+    },
+    methods:{
+    //方法
+    }
+})
