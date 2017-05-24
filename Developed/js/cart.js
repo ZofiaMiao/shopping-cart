64636f25e68e54cf678a1bed2fd036abc4f439f9
zofina <izhmaple@163.com>
2017-05-24 13:22:47 +0800

    v-for渲染商品列表

--- Developed/js/cart.js
+++ Developed/js/cart.js
@@ -3,19 +3,29 @@
  */
 var vm = new Vue({
     el:"#app",
-    ata:{
+    data:{
     //数据
-
+        productList:[],
+        totalMoney:0
     },
     fiters:{
     //过滤器
-        
+
     },
     mounted:function () {
     //挂载到实例上去之后调用该钩子
-
+        this.cartView();
     },
     methods:{
-    //方法
+    // 方法
+        cartView:function () {
+            var _this = this;
+            this.$http.get("data/cartData.json").then(function (res) {
+                _this.productList = res.data.result.list;
+                _this.totalMoney = res.data.result.totalMoney;
+                console.log(_this.productList);
+                console.log(_this.totalMoney);
+            });
+        }
     }
 })
