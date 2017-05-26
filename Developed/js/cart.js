e20de1cc156de95695dadaeb6280b6bc33649847
zofina <izhmaple@163.com>
2017-05-26 13:18:30 +0800

    过滤器显示金额

--- Developed/js/cart.js
+++ Developed/js/cart.js
@@ -1,31 +1,38 @@
 /**
  * Created by zofia on 17/5/23.
  */
-new Vue({
+var vm = new Vue({
     el:"#app",
     data:{
     //数据
         productList:[],
         totalMoney:0
     },
-    fiters:{
-    //过滤器
-
+    filters:{
+    //局部过滤器
+        fomartMoney:function (value) {
+            return "¥"+value.toFixed(2);
+        }
     },
     mounted:function () {
     //挂载到实例上去之后调用该钩子
-        this.cartView();
+        this.$nextTick(function () {
+            vm.cartView();
+        })
+
     },
     methods:{
     // 方法
         cartView:function () {
-            var _this = this;
-            this.$http.get("data/cartData.json").then(function (res) {
-                _this.productList = res.data.result.list;
-                _this.totalMoney = res.data.result.totalMoney;
-                console.log(_this.productList);
-                console.log(_this.totalMoney);
+            this.$http.get("data/cartData.json").then(res=>{
+                //es6箭头函数，作用域指向外层
+                this.productList = res.data.result.list;
+                this.totalMoney = res.data.result.totalMoney;
             });
         }
     }
+})
+//全局过滤器
+Vue.filter("money",function (value, type) {
+    return "¥"+value.toFixed(2)+type;
 })
